6d3ef7bfce550db3c5660ea4bec4ba22fb1589bc
luisb <luisbiel2017sg@gmail.com>
2025-07-09 09:47:01 -0300

    Error exception

--- trabalhoPratico/src/main/java/servicos/BancoReservas.java
+++ trabalhoPratico/src/main/java/servicos/BancoReservas.java
@@ -19,16 +19,19 @@ import java.util.List;
 public class BancoReservas {
     private static final List<EspacoFisico> reservas = new ArrayList<>();
     
-public static void adicionarReserva(EspacoFisico novaReserva) throws HorarioIndisponivelException {
-    for (EspacoFisico reserva : reservas) {
-        if (reserva.getData().equals(novaReserva.getData())
-                && reserva.getNome().equals(novaReserva.getNome())
-                && reserva.getHorario().equals(novaReserva.getHorario())) {
-            throw new HorarioIndisponivelException("Horário já reservado para este espaço.");
+public static void adicionarReserva(String caminhoArquivo) throws HorarioIndisponivelException {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoArquivo))) {
+        for (EspacoFisico reserva : reservas) {
+            String linha = "Data: " + reserva.getData()
+                    + " | Espaço: " + reserva.getNome()
+                    + " | Horário: " + reserva.getHorario();
+            writer.write(linha);
+            writer.newLine();
         }
+        System.out.println("Reservas exportadas com sucesso para: " + caminhoArquivo);
+    } catch (IOException e) {
+        System.err.println("Erro ao exportar reservas: " + e.getMessage());
     }
-
-    reservas.add(novaReserva);
 }
     
     public static List<EspacoFisico> getReservas() {
